7fe4cbd0b07b5860a114494e0032627f9c39ff3f
huangbingyi <byhuang1998@gmail.com>
2023-02-08 15:49:40 +0800

    master design pattern singleton 懒汉式

---
+++ pattern/src/main/java/com/byhuang/singleton/lazy/Singleton.java
@@ -0,0 +1,22 @@
+package com.byhuang.singleton.lazy;
+
+/**
+ * @author mskj-huangbingyi
+ * @version 1.0
+ * @date 2023/2/8 15:46
+ * @description 懒汉式
+ */
+public class Singleton {
+
+    private Singleton() {
+    }
+
+    private static Singleton instance;
+
+    public static synchronized Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
---
+++ pattern/src/main/java/com/byhuang/singleton/lazy/Test.java
@@ -0,0 +1,19 @@
+package com.byhuang.singleton.lazy;
+
+/**
+ * @author mskj-huangbingyi
+ * @version 1.0
+ * @date 2023/2/8 15:47
+ * @description TODO
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Singleton instance1 = Singleton.getInstance();
+        Singleton instance2 = Singleton.getInstance();
+
+        System.out.println(instance1);
+        System.out.println(instance2);
+        System.out.println(instance1 == instance2);
+    }
+}
