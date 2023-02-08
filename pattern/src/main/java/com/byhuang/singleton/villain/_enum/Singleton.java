ae609544b59627357a2cbaa35faba6697080bbf8
huangbingyi <byhuang1998@gmail.com>
2023-02-08 22:01:14 +0800

    master design pattern singleton enum

---
+++ pattern/src/main/java/com/byhuang/singleton/villain/_enum/Singleton.java
@@ -0,0 +1,11 @@
+package com.byhuang.singleton.villain._enum;
+
+/**
+ * @author huangbingyi
+ * @version 1.0
+ * @date 2023/2/8 21:57
+ * @description TODO
+ */
+public enum Singleton {
+    INSTANCE;
+}
---
+++ pattern/src/main/java/com/byhuang/singleton/villain/_enum/Test.java
@@ -0,0 +1,21 @@
+package com.byhuang.singleton.villain._enum;
+
+/**
+ * @author huangbingyi
+ * @version 1.0
+ * @date 2023/2/8 15:47
+ * @description TODO
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        Singleton singleton1 = Singleton.INSTANCE;
+        Singleton singleton2 = Singleton.INSTANCE;
+
+        System.out.println(singleton1);
+        System.out.println(singleton2);
+        System.out.println(singleton1 == singleton2);
+
+    }
+}
