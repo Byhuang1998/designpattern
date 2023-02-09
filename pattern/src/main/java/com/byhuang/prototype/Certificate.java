27127e4c40eb06584bb2b0278a50d4a15a765689
huangbingyi <byhuang1998@gmail.com>
2023-02-09 15:36:20 +0800

    master design pattern prototype 原型模式

---
+++ pattern/src/main/java/com/byhuang/prototype/Certificate.java
@@ -0,0 +1,29 @@
+package com.byhuang.prototype;
+
+/**
+ * @author mskj-huangbingyi
+ * @version 1.0
+ * @date 2023/2/9 15:22
+ * @description TODO
+ */
+public class Certificate implements Cloneable {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return (Certificate) super.clone();
+    }
+
+    public void show() {
+        System.out.println(name + "获得了奖状");
+    }
+}
---
+++ pattern/src/main/java/com/byhuang/prototype/Test.java
@@ -0,0 +1,21 @@
+package com.byhuang.prototype;
+
+/**
+ * @author mskj-huangbingyi
+ * @version 1.0
+ * @date 2023/2/9 15:26
+ * @description TODO
+ */
+public class Test {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Certificate certificate1 = new Certificate();
+        Certificate clone = (Certificate) certificate1.clone();
+
+        certificate1.setName("张三");
+        clone.setName("李四");
+
+        certificate1.show();
+        clone.show();
+    }
+}
