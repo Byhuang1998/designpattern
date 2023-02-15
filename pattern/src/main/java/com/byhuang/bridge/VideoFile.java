3f843e031007361fe7b6d221d895d69fbb4637bc
huangbingyi <byhuang1998@gmail.com>
2023-02-15 22:01:57 +0800

    master design pattern bridge 桥接模式

---
+++ pattern/src/main/java/com/byhuang/bridge/VideoFile.java
@@ -0,0 +1,13 @@
+package com.byhuang.bridge;
+
+/**
+ * @version v1.0
+ * @ClassName: VideoFile
+ * @Description: 视频文件(实现化角色)
+ * @Author: huangbingyi
+ */
+public interface VideoFile {
+
+    //解码功能
+    void decode(String fileName);
+}
